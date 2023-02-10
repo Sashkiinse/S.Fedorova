bf5af8f57d781e19534932b4e53e093425c90b29
Sashkiinse <124045314+Sashkiinse@users.noreply.github.com>
2023-02-10 10:36:06 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+my_list = [True, 786, 3.14, 'text', 70.2]
+second_list = [123, 'text']
+print(my_list)
+print(my_list[0])
+print(my_list[1:3])
+print(my_list[2:])
+print(second_list *2)
+print(my_list + second_list)
+
+
+
+my_tuple = (True, 786, 3.14, 'text', 70.2)
+second_tuple = (123, 'text')
+print(my_tuple)
+print(my_tuple[0])
+print(second_tuple*2)
+print(my_tuple + second_tuple)
+
+
+my_dict = {}
+my_dict["country"]="Mexico"
+print(my_dict["country"])
+another_dict= {"number":23, 2: True, "my_list":[1,2,3]}
+print(another_dict.keys())
+print(another_dict.values())
