fe90a5c6847a8216cb6540956ec1b3662686a622
Sashkiinse <124045314+Sashkiinse@users.noreply.github.com>
2023-02-17 09:20:26 +0300

    ДЗ на 17.02.2023

--- main (6).py
+++ main (6).py
@@ -1,7 +1,6 @@
-dark=input("Вы хотите спать (да, нет, не знаю)")
-if dark == "Да":
-    print("Идите спать")
-elif dark=="не знаю":
-    print('подумайте еще')
-else:
-    print('Идите гулять')
+x=int(input())
+if x<0:
+    print("Меньше 0")
+y=int(input())
+if y>0:
+    print("Больше 0")
