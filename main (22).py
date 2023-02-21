a7d7768555bf3e6542af80f3b734dbf960cb8605
Sashkiinse <124045314+Sashkiinse@users.noreply.github.com>
2023-02-21 13:37:08 +0300

    Дз на 21.02.2023

---
+++ main (22).py
@@ -0,0 +1,14 @@
+a = input ('a = ')
+b = input ('b = ')
+if a > b:
+  c = a - b
+  print ('c')
+elif a < b:
+  c = a + b
+  print ('c')
+elif a != b:
+    c = a*b
+else:
+  c = a
+  print ('c')
+input()
