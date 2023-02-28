6c5a913f4812fed85b130ca8ed8200a374f9c4f1
Sashkiinse <124045314+Sashkiinse@users.noreply.github.com>
2023-02-28 13:22:54 +0300

    дз задание  на 28.02.23

--- main (5).py
+++ main (5).py
@@ -1,16 +1,7 @@
-a = 1
-b = 2
-print((a>b) and (b>a))
-print((b>a) and (a<b))
-print((a>b) or (b>a))
-print(not (b>a))
-c = a + b
-print((c > b) or (b>a))
-print((a>c) or (b>c))
-print((c<b) or (b<a))
-print((a<c) or (b>a))
-f = 'sasha'
-q = 'semen'
-print((f > q) and (q>f))
-print((q>f) or (q<f))
-print(not(f<q))
+a = int(input('Введите трехзначное число '))
+s = a // 100;
+d = (a - s*100) // 10;
+e = a - s*100 - d*10;
+b = e*100 + d*10 + s;
+r = a - b;
+print(r)
--- main (6).py
+++ main (6).py
@@ -1,6 +1,3 @@
-x=int(input())
-if x<0:
-    print("Меньше 0")
-y=int(input())
-if y>0:
-    print("Больше 0")
+n = int(input())
+while n > 0: 
+    print(n % 10); n //= 10
