56af56646a840a9e02c948eb6c583205e59a18b1
Sashkiinse <124045314+Sashkiinse@users.noreply.github.com>
2023-03-03 10:07:25 +0300

    Классная р. 03,03,2023

--- main (20).py
+++ main (20).py
@@ -1,10 +1,5 @@
-a = int(input())
-b = int(input())
-c = 0
-if a > b:
-    c = a-b
-elif a == b:
-    c = a
-else:
-    c = a + b
-print(c)
+n=int(input("Ввод первого числа"))
+m=int(input("Ввод второго числа"))
+A=(n+m)/2
+B=(n-m)**(1/2)
+print (A,B)
