dea47202f7d8c704a8cf86b25e821a826879c18c
Sashkiinse <124045314+Sashkiinse@users.noreply.github.com>
2023-03-07 13:59:00 +0300

    Дз на 07.03.2023

--- main (6).py
+++ main (6).py
@@ -1,3 +1,8 @@
-n = int(input())
-while n > 0: 
-    print(n % 10); n //= 10
+radius = int(input('Введите радиус круга')) 
+kvadrat = int(input('Введите длинну стороны квадрата')) 
+ploshad1 = 3.14 * radius * radius 
+ploshad2 = kvadrat * kvadrat 
+if ploshad1 > ploshad2: 
+ print('площадь круга больше площади квадрата') 
+else: 
+ print('площадь круга меньше площади квадрата')
