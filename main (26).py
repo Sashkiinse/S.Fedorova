cd1cc0162c22d81adffe37a217680b214c84310f
Sashkiinse <124045314+Sashkiinse@users.noreply.github.com>
2023-03-14 14:29:26 +0300

    классная работа 14.03.23

---
+++ main (26).py
@@ -0,0 +1,12 @@
+x = int(input())
+y = int(input())
+c = int(input())
+
+sin x = (c*y)/(x*y)
+cos x = (x*c)/(x*y)
+if sin x <= (3.14/4):
+    y = sin x 
+    print(y)
+else: 
+    y = cos x
+    print (y)
