4fcd396e9ad0ab5ef618ee7f1e8ea1d493ebcf05
Sashkiinse <124045314+Sashkiinse@users.noreply.github.com>
2023-05-04 22:02:05 +0300

    дз 04.05.23

--- main (7).py
+++ main (7).py
@@ -1,9 +1,15 @@
-num1 = input() 
-num2 = input() 
-if num1 > num2: 
- max = num1 
- min = num2 
-else: 
- max = num2 
- min = num1 
-print('Большее число -',max, 'Меньшее число -',min)
+import random 
+random_value = random.randint (1, 100)
+attempt=0
+print("компьютер загадал число от 1 до 100 и у вас 10 попыток")
+for i in range(1,11):
+    choice= int(input("введите число"))
+    if choice > random_value:
+        print("много")
+    elif choice < random_value:
+        print("мало")
+    else:
+        print(f"вы угадали число с {i}-ой попытки")
+        
+    
+                
