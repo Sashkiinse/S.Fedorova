f5142916930b25dbf3d17def2cdb90ad05d4c273
Sashkiinse <124045314+Sashkiinse@users.noreply.github.com>
2023-06-02 10:15:52 +0300

    Add files via upload

---
+++ main (47).py
@@ -0,0 +1,9 @@
+def is_prime(x):
+    f=True
+    for i in range(2, int(x**0.5+1)):
+        if x%i ==0:
+            f=False
+    return(f)
+a+int(input('Ввести число: '))
+print(is_prime(a))
+is_prime(x)
